30951587eda8d6c91c930f8663f12b0f0dba4f8a
notzie <notykt@gmail.com>
2021-07-28 18:36:48 +0900

    refactor TokenValidation

--- MobileUpGallery/Services/TokenValidationService.swift
+++ MobileUpGallery/Services/TokenValidationService.swift
@@ -1,19 +1,13 @@
-//
-//  TokenValidation.swift
-//  MobileUpGallery
-//
-//  Created by Erkhaan on 28.07.2021.
-//
-
 import Foundation
 import Alamofire
 
-class TokenValidation{
-	var token: String
+class TokenValidationService{
+
 	private let accessToken = "838356648383566483dda3e29a83fbe8d88838383835664e283aa1195f872b664b6e28a"
 	private let clientSecret = "RVBI3qVIVwYDPS7lBv3P"
 	private var link: String
 	private var url: URL?
+	var token: String
 
 	init(token: String){
 		self.token = token
@@ -22,16 +16,19 @@ class TokenValidation{
 	}
 
 	func validateToken(completion: @escaping (Bool) -> Void){
+
 		guard let url = url else{
 			print("wrong url")
 			return
 		}
 		var ans: Bool = false
+
 		AF.request(url).responseDecodable(of: ValidationWelcome.self){response in
 			switch response.result{
 			case .success(let value):
 				if(value.response.success == 1){
 					if let expire = value.response.expire{
+						// if token expires in hour, then update
 						let delta = (Date(timeIntervalSince1970: TimeInterval(expire)) - Date())/(60*60)
 						if(delta > 1){
 							ans = true
