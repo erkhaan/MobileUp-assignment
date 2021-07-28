0ac6b590c2514e8ef9f94686551bf9e369931d55
notzie <notykt@gmail.com>
2021-07-28 17:34:44 +0900

    delete comments from ApiService

--- MobileUpGallery/Services/API.swift
+++ MobileUpGallery/Services/API.swift
@@ -1,10 +1,3 @@
-//
-//  API.swift
-//  MobileUpGallery
-//
-//  Created by Erkhaan on 28.07.2021.
-//
-
 import Foundation
 import Alamofire
 
@@ -14,19 +7,12 @@ class ApiService{
 		let url = "https://api.vk.com/method/photos.get?owner_id=-128666765&album_id=266276915&access_token=\(token)&v=5.131"
 
 		AF.request(url).responseDecodable(of: Welcome.self) { response in
-			//debugPrint("Response: \(response)")
 			switch response.result{
 			case .success(let value):
-				//print("yay")
 				completion(value.response.items)
 			case .failure(let error):
 				print(error)
 			}
 		}
-			/*guard let articles = response.value else{
-				print("article download error")
-				return
-			}
-			completion(articles.articles)*/
 	}
 }
